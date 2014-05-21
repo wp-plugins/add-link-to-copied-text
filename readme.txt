c2aeb7c10ea142dcf66ba0585f85da36a3b56d2b
amitaits <amitaits@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-21 00:52:44 +0000

    Added files to trunk
    git-svn-id: https://plugins.svn.wordpress.org/add-link-to-copied-text/trunk@918301 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Add Link to Copied Text ===
+Contributors: amitaits, kka284556
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=amitsonkhiya%40hotmail%2ecom&lc=US&item_name=Fellow%20Tuts%20%2d%20Plugin%20Development%20Support&no_note=0&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
+Tags: copied, copy, copyright, protect, stop, content, website, link, add, user, users, visitor, visitors, text, plugin, wordpress
+Requires at least: 3.4
+Tested up to: 3.9
+Stable tag: 1.0
+License: GPLv2 or later
+
+This plugin automatically add a link to your website/page beneath copied text from your website to the page wherever visitors paste. This powerful plugin can also protect visitors to copy your content and works on almost all browsers
+
+== Description ==
+
+Add Link to Copied Text plugin is capable to add a link to your website/page along with your desirous setting whenever a visitor copy content from your website. Alternatively you can also stop visitor to copy your content or replace copied text with your custom text.
+
+You can visit our <a href="http://dev.fellowtuts.com/add-link-to-copied-text-plugin/">Add Link to Copied Text page</a> to check available options with this powerful plugin.
+
+We have tested this plugin with all major browsers including chrome, firefox, opera, safari and IE version 7 to 11.
+
+<strong>Don't forget to switch to another page before you copy content to see it in action after making any change in this plugin settings</strong>.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Install Add Link to Copied Text plugin either via the WordPress.org plugin directory, or by uploading the add_link_on_copy folder to `/wp-content/plugins/` directory at your server
+2. Activate the plugin through the 'Plugins' menu in WordPress Dashboard
+3. Go to 'Settings' => 'Add Link' menu under your Dashboard  
+4. Customize the setting as per your requirement and you're ready
+
+
+== Frequently Asked Questions ==
+
+= Where is plugin options page? =
+Options are available under 'Settings' => 'Add Link' menu in your Dashboard.
+
+= Where to find more information related to options? =
+Options are self-explanatory and you can visit <a href="http://dev.fellowtuts.com/add-link-to-copied-text-plugin/">Add Link to Copied Text page</a> to have more information
+
+= Does this plugin work with Internet Explorer? =
+We have tested this plugin with Internet Explorer versions 7, 8, 9, 10, 11 and other browsers like chrome, opera, safari, firefox as well
+
+== Screenshots ==
+1. plugin options
+
+== Changelog ==
+
+= 1.0 =
+* Let you enable to control when a visitor copy content from your website
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial Release
---
+++ settings.php
@@ -0,0 +1,189 @@
+<?php
+/*
+Plugin Name: Add Link to Copied Text
+Plugin URI: http://dev.fellowtuts.com/add-link-to-copied-text-plugin/
+Description: This plugin automatically add a link to your website/page beneath copied text from your website to the page wherever visitors paste. This powerful plugin can also protect visitors to copy your content and works on almost all browsers
+Version: 1.0
+Author: Amit Sonkhiya, Kamal Agrawal 
+Author URI: http://dev.fellowtuts.com
+License: GPLv2 or later
+*/
+
+if ( ! class_exists( 'ftAddlink' ) ){
+class ftAddlink {
+	
+	private $options;
+	private $option_name = 'ftAddlink_options';
+	
+	// constructor
+	function ftAddlink() {
+		
+		
+		$this->options = get_option($this->option_name);		
+		
+		add_action( 'admin_menu', array( &$this, 'ftAddlink_menu' ) );
+		add_action( 'admin_init', array( &$this, 'ftAddlink_admininit' ) );
+		
+		add_action( 'init', array( &$this, 'ftAddlink_init') );
+		add_action( 'wp_head', array( &$this, 'add_script' ) );
+	}
+	
+	
+	
+	function ftAddlink_init(){
+		
+		$options = $this->options ; 
+		
+		if( !isset($options['readmore']) ) $options['readmore'] = 'Continue reading at:';
+		if( !isset($options['breaks']) ) $options['breaks'] = 2;
+		if( !isset($options['usetitle']) ) $options['usetitle'] = false;
+		if( !isset($options['addlinktosite']) ) $options['addlinktosite'] = false;		
+		if( !isset($options['cleartext']) ) $options['cleartext'] =  false;
+		if( !isset($options['addsitename']) ) $options['addsitename'] = true;		
+		if( !isset($options['replaced_text']) ) $options['replaced_text'] = '';
+		
+		
+	}
+	
+	function add_script() {
+		wp_register_script( 'add_linkoncopy',  plugins_url( basename( dirname( __FILE__ ) ) ) . '/assets/add_link.js');
+		wp_enqueue_script( 'add_linkoncopy' );
+		
+		global $post;
+		
+			
+		$options = $this->options;
+		
+		$params = 	array(
+			
+			 'readmore'	    	=> $options['readmore'],
+			 'breaks'		    => $options['breaks'],
+			 'addlinktosite'	=> $options["addlinktosite"] ,
+			 'usetitle'			=> $options['usetitle'],			
+			 'cleartext'		=> $options['cleartext'],
+			 'addsitename'		=> $options['addsitename'],			
+			 'replaced_text'	=> $options['replaced_text'],
+			 'sitename'			=> get_bloginfo('name'),
+			 'siteurl'			=> get_bloginfo('url')			 
+		);
+		
+		if ($options['usetitle'] === true) {
+			
+			if (is_home() || is_front_page()){
+				
+				$params['pagetitle'] = get_bloginfo('name');
+				$params['addsitename'] = false;
+			}
+			if (is_singular()){
+				$params['pagetitle'] = get_the_title($post->ID);
+			}
+		}
+		wp_localize_script( 'add_linkoncopy', 'add_link', $params );
+	}
+	
+	// adding menu item in admin menu
+	function ftAddlink_menu(){
+		
+		add_options_page('Add link Settings', 'Add Link', 'manage_options', 'ftAddlink_options',array($this, 'ftAddlink_display_settings'));
+		
+	}
+	
+	//register_setting( $option_group, $option_name, $sanitize_callback );
+	function ftAddlink_admininit()
+	{
+		register_setting($this->option_name, $this->option_name, array($this, 'options_validate'));		
+	}
+		
+	public function options_validate($input) {
+
+    $valid = array();
+	$valid = $input;
+	
+    $valid['readmore'] = sanitize_text_field($input['readmore']);
+    $valid['breaks'] = sanitize_text_field($input['breaks']);
+	$valid['addlinktosite'] = isset($input['addlinktosite']) ? (bool) $input['addlinktosite'] : false;
+	$valid['usetitle'] = isset($input['usetitle']) ? (bool) $input['usetitle'] : false;
+	$valid['cleartext'] = isset($input['cleartext']) ? (bool) $input['cleartext'] : false;
+	$valid['addsitename'] = isset($input['addsitename']) ? (bool) $input['addsitename'] : false;
+	$valid['replaced_text'] = $input['replaced_text'];
+	
+
+    if (strlen($valid['breaks']) == 0 || $valid['breaks'] < 0) {
+        add_settings_error(
+                'breaks',                     // Setting title
+                'breaks_texterror',            // Error ID
+                'Please enter a valid integer number',     // Error message
+                'error'                         // Type of message
+        );
+
+        // Set it to the default value
+        $valid['breaks'] = 0;
+    }   
+
+    return $valid;
+}
+
+	function ftAddlink_display_settings(){
+		
+		$options = get_option($this->option_name);
+		//default values
+		$readmore = isset($options['readmore'])? $options['readmore'] : 'Continue reading at';
+		$breaks = isset($options['breaks'])  ? $options['breaks'] : 2;
+   ?>
+<style>
+.form-table th {width:40%}
+</style>
+<div class="wrap">
+  <h2>Add Link On Copy Options</h2>
+  <form method="post" action="options.php">
+    <?php settings_fields($this->option_name); ?>
+    <table class="form-table">
+      <tr valign="top">
+        <th scope="row">Label to append: <!--<br /><small>(eg: "Continue reading at")</small>--></th>
+        <td>
+        <input type="text" name="<?php echo $this->option_name ?>[readmore]" value="<?php echo $readmore; ?>" /></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row">Number of &lt;br /&gt; tags to insert before the link:<!-- <br /><small>(default: 2)</small> --></th>
+        <td><input type="text" name="<?php echo $this->option_name?>[breaks]" value="<?php echo $breaks; ?>" /></td>
+      </tr>
+      <tr valign="top">
+        <th scope="row">Link to site instead of page/post:</th>
+        <td><input type="checkbox" name="<?php echo $this->option_name?>[addlinktosite]" <?php checked($options['addlinktosite']); ?>  /></td>
+      </tr>
+       <tr valign="top">
+        <th scope="row">Use page/post title as link text:</th>
+        <td><input type="checkbox" name="<?php echo $this->option_name?>[usetitle]" <?php checked($options['usetitle']); ?>  /></td>
+      </tr>
+      
+       <tr valign="top">
+        <th scope="row">Add site title to link text:</th>
+        <td><input type="checkbox" name="<?php echo $this->option_name?>[addsitename]" <?php checked($options['addsitename']); ?>  /></td>
+      </tr>
+      
+      <tr valign="top">
+        <th scope="row">Replace copied text with:</th>
+        <td><textarea name="<?php echo $this->option_name?>[replaced_text]" rows="5" cols="50"><?php echo $this->options['replaced_text']?></textarea>
+        </td>
+      </tr>
+      
+      <tr valign="top">
+        <th scope="row">OR<br /><br /><span style="color: Red;">Don't let user copy my content:</span></th>
+        <td><br /><br /><input type="checkbox" name="<?php echo $this->option_name?>[cleartext]" <?php checked($options['cleartext']); ?>  /></td>
+      </tr>      
+    </table>
+    <p class="submit">
+      <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+  </form>
+</div>
+<?php
+		
+		}
+} // class ends here
+
+$ftAddlink = new ftAddlink();
+
+}// top most if condition ends here
+
+
